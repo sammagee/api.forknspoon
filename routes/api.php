2c0fe6ef378f740314dd0a1481b5c442422f9454
Sam Magee <s@sammagee.me>
2021-11-28 16:51:03 -0600

    adds fetch endpoint for recipes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\FetchRecipesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/recipes', FetchRecipesController::class);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
